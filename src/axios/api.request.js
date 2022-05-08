2c198d6af142f3c8fdd72c68ba58a01dbd3fc827
PY <181250111@smail.nju.edu.cn>
2022-05-08 23:54:26 +0800

    feat: 部署

--- src/axios/api.request.js
+++ src/axios/api.request.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 
-const baseUrl = process.env.NODE_ENV === 'development' ? 'http://localhost:8001' : '' //部署地址待补充
+const baseUrl = process.env.NODE_ENV === 'development' ? 'http://172.29.7.224:8001' : 'http://172.29.7.224:8001' //部署地址待补充
 
 const HttpRequest = axios.create({
   baseURL: baseUrl,  // api的base_url
--- src/axios/index.js
+++ src/axios/index.js
@@ -3,7 +3,7 @@ export default {
    * @description api请求基础路径
    */
   baseUrl: {
-    dev: 'http://localhost:8001',
-    pro: '' //部署地址待补充
+    dev: 'http://172.29.7.224:8001',
+    pro: 'http://172.29.7.224:8001' //部署地址待补充
   }
 }
