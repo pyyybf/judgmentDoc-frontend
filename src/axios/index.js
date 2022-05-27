47ef168e5e15a5a8341c01b5e376b8c4fa5adbbd
pyyybf <3052899600@qq.com>
2022-05-28 02:23:31 +0800

    feat: 配置本地/部署地址

--- src/axios/index.js
+++ src/axios/index.js
@@ -3,7 +3,7 @@ export default {
    * @description api请求基础路径
    */
   baseUrl: {
-    dev: 'http://172.29.7.224:8001',
+    dev: 'http://localhost:8001',
     pro: 'http://172.29.7.224:8001' //部署地址待补充
   }
 }
