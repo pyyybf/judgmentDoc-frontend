2c0542fd21df966882784ff8063b39fc8e01542a
pyyybf <3052899600@qq.com>
2022-05-28 02:23:51 +0800

    feat: 增大请求超时时间

--- src/axios/api.request.js
+++ src/axios/api.request.js
@@ -4,7 +4,7 @@ const baseUrl = process.env.NODE_ENV === 'development' ? 'http://localhost:8001'
 
 const HttpRequest = axios.create({
   baseURL: baseUrl,  // api的base_url
-  timeout: 30000,  // 请求超时时间
+  timeout: 120000,  // 请求超时时间
   withCredentials: true
 });
 
